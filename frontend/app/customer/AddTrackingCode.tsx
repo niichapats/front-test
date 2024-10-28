2311a3d462e1eb5157054aed9321998d1915357e
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-29 02:37:19 +0700

    New style

--- frontend/app/customer/AddTrackingCode.tsx
+++ frontend/app/customer/AddTrackingCode.tsx
@@ -57,44 +57,63 @@ const AddTrackingCode: React.FC = () => {
     return (
         <>
             {/* Input Section */}
-            <div className="join">
-                <h1 className="text-3xl text-amber-700 font-semibold mt-10 ml-20 mr-10">Enter Tracking Code</h1>
-                <input 
-                    className="input input-bordered join-item border-stone-300 w-96 mt-10"
-                    placeholder="Tracking Code" 
-                    value={trackingCode}
-                    onChange={handleTrackingCodeChange}
-                />
-                <button className="btn bg-amber-700 text-white join-item mt-10" onClick={() => handleEnterClick(trackingCode)}>Enter</button>
+            <div className='mt-10 text-center'>
+                <h1 className="text-3xl text-amber-700 font-semibold mt-10 ml-20 mr-10 ">Enter Tracking Code</h1>
+            </div>
+            <div className="text-center">
+                <div className="join">
+                    <input 
+                        className="input input-bordered join-item border-stone-300 w-96 mt-10"
+                        placeholder="Tracking Code" 
+                        value={trackingCode}
+                        onChange={handleTrackingCodeChange}
+                    />
+                    <button className="btn bg-amber-700 text-white join-item mt-10" onClick={() => handleEnterClick(trackingCode)}>Enter</button>
+                </div>
             </div>
 
             {/* Error Message */}
             {error && 
-                <div className="text-red-500 text-lg font-semibold mt-5 mb-5 ml-20 mr-20 flex items-center justify-center bg-red-100 p-4 rounded-lg">
+                <div className="flex justify-center mt-5">
+                <div className="text-red-500 text-lg font-semibold mb-5 ml-20 mr-20 bg-red-100 p-4 rounded-lg w-96">
                     <p>{error}</p>
                 </div>
+            </div>
+            
             }
 
             {/* Display Queue Info after Button Click */}
             {postedData.length > 0 && (
+                <div className='flex justify-center'>
                 <div className="mt-10 ml-20 mr-20">
                     {postedData.map(item => (
-                        <div key={item.id} className="mb-5 card bg-orange-50 p-6 rounded-lg shadow-lg w-full">
-                            <div className="flex justify-between">
+                        <div key={item.id} className="mb-5 card bg-orange-50 p-6 rounded-lg shadow-lg w-96 h-96">
+                            <div className="text-center">
                                 <div>
-                                    <h3 className="text-xl text-orange-900 font-semibold">
+                                    <h3 className="text-2xl text-orange-900 font-semibold">
                                         {item.business} ({item.name})
+                                        <br />
+                                        <br />
                                     </h3>
-                                    <p className="text-sm text-orange-700 font-semibold">
-                                        Time In: {item.time_in}, Time Out: {item.time_out || 'N/A'}
+                                    <h1 className="text-9xl text-orange-900">
+                                        {item.queue_ahead}
+                                    </h1>
+                                    <p className="text-base text-orange-900 font-semibold">
+                                        Ahead of you
                                     </p>
-                                    <p className="text-sm text-orange-500 font-semibold">
+                                    <p className="text-base text-orange-700 font-semibold">
+                                        <br />
+                                        <br />
+                                        Time In: {item.time_in}
+                                    </p>
+                                    <p className="text-base text-orange-500 font-semibold">
                                         Status: {item.status}
                                     </p>
                                 </div>
                             </div>
                         </div>
                     ))}
+                </div>
                 </div>
             )}
         </>
