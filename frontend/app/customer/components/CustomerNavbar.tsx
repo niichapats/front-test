cc1abaa7ec6ee1f7014d7aad7cca3501bbc5a9c2
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-30 20:23:27 +0700

    New Navbar

--- frontend/app/customer/components/CustomerNavbar.tsx
+++ frontend/app/customer/components/CustomerNavbar.tsx
@@ -4,30 +4,27 @@ import React from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
-
-export default function CustomerNavBar() {
+export default function BusinessNavbar() {
     const pathname = usePathname()
+
     return (
-        <div className='navbar fixed z-50'>
+        <div className='navbar fixed z-50 bg-darkPurple max-w-full'>
             <div className='flex-1'>
-                <Image src="/logo2.png" width={220} height={200} alt='logo' />
+                <Link href="/"><Image src="/logo.png" width={120} height={120} alt='logo'></Image></Link>
             </div>
             <div className='flex-none'>
-                <ul className='menu menu-horizontal px-1'>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
+                <ul className='btn menu menu-horizontal px-1 bg-white rounded-full h-10 flex justify-center items-center'>
                     <li>
                         <details>
-                            <summary>Account</summary>
-                            <ul className='bg-base-100 rounded-t-none p-2'>
+                            <summary className='font-bold flex items-center justify-center h-full'>Account</summary>
+                            <ul className='absolute left-0 bg-base-100 mt-2 rounded-lg p-2 shadow-md'>
                             <li>
                                 <Link href="/profile">
                                     Profile
                                 </Link>
                             </li>
                             <li>
-                                <Link href="/customer/login">
+                                <Link href="/business/logout">
                                     Logout
                                 </Link>
                             </li>
