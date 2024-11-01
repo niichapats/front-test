19441fa804174e2cd137e647c46737c621ab8d66
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-01 10:57:30 +0700

    Add color

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,12 +11,29 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightBlue: '#D1E9F6',
+        lightPurple1: '#F7DBF0',
+        lightPurple2: '#F4EEFF',
+        lightkPurple3: '#AB95D6',
+        lightSky: '#BFECFF',
+        lightYellow: '#FFF8C9',
+        darkGrey1: '#27374D',
+        darkGrey2: '#526D82',
+        lightGrey1: '#9DB2BF',
+        lightGrey2: '#DDE6ED',
+        darkPurple: '#424874',
+        darkPurple2: '#704BAB',
+        cream: '#FAF0E2',
+        lightPink: '#DB9ABD'
       },
       height: {
+        '26': '4rem',
+        '60': '16rem',
         '76': '20rem',
         '100': '35rem',
       },
       width: {
+        '32': '8rem',
         '100': '40rem'
       }
     },
